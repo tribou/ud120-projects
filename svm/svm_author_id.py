d6ed8aae6bf2e8213730e33e54519cc094d0f17f
Aaron Tribou <aaron.tribou@gmail.com>
2015-06-02 20:15:30 -0500

    Saving progress halfway through

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -12,9 +12,11 @@
     
 import sys
 from time import time
+import os
+dir = os.path.dirname(os.path.realpath(__file__))
 sys.path.append("../tools/")
 from email_preprocess import preprocess
-
+from ..choose_your_own import prettyPicture
 
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
@@ -26,7 +28,12 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+from sklearn import svm
+clf = svm.SVC(kernel="linear")
+clf.fit(features_train, labels_train)
+pred = clf.predict(features_test)
 
+from sklearn.metrics import accuracy_score
+accuracy_score(labels_test, pred)
 #########################################################
-
 
