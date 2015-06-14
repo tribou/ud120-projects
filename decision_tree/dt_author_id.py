b64132007516295462110ea949163bc8eb45557a
Aaron Tribou <aaron.tribou@gmail.com>
2015-06-14 12:44:36 -0500

    Calculate decision tree with 40 min_samples_split

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -30,15 +30,15 @@ features_train, features_test, labels_train, labels_test = preprocess()
 from sklearn import tree
 clf = tree.DecisionTreeClassifier(min_samples_split=40)
 
-print("features:",len(features_train[0]))
+#print("features:",len(features_train[0]))
 
-#t0 = time()
-#clf.fit(features_train, labels_train)
-#pred = clf.predict(features_test)
-#print "predicting time:", round(time()-t0, 3), "s"
-#
-#from sklearn.metrics import accuracy_score
-#print(accuracy_score(labels_test, pred))
+t0 = time()
+clf.fit(features_train, labels_train)
+pred = clf.predict(features_test)
+print "predicting time:", round(time()-t0, 3), "s"
+
+from sklearn.metrics import accuracy_score
+print(accuracy_score(labels_test, pred))
 
 #########################################################
 
